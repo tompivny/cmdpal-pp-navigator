8e98b1cb35619d1ab85f29a7aa6f53b3804a805b
tompivny <tomas@pivny.cz>
2026-03-01 16:08:24 +0100

    init

---
+++ PowerPlatformNavigator/PowerPlatformNavigatorCommandsProvider.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace PowerPlatformNavigator;
+
+public partial class PowerPlatformNavigatorCommandsProvider : CommandProvider
+{
+    private readonly ICommandItem[] _commands;
+
+    public PowerPlatformNavigatorCommandsProvider()
+    {
+        DisplayName = "Power Platform Navigator";
+        Icon = IconHelpers.FromRelativePath("Assets\\StoreLogo.png");
+        _commands = [
+            new CommandItem(new PowerPlatformNavigatorPage()) { Title = DisplayName },
+        ];
+    }
+
+    public override ICommandItem[] TopLevelCommands()
+    {
+        return _commands;
+    }
+
+}
